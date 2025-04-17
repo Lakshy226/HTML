49c246ddaa2e62d9a1a7a52f515f300844ae4abd
Lakshy Chandwani <lakshychandwani22@gmail.com>
2025-04-17 11:01:28 +0530

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,104 @@
+/*function power(base, exponent = 2){
+    return Math.pow(base,exponent);
+}
+console.log(power(6));
+
+"use strict";
+let strNum ="41.88";
+console.log(parseInt(strNum));
+console.log(parseFloat(strNum));
+
+let num = 197.759;
+console.log(num.toFixed(2));
+console.log(num.toPrecision(4));
+console.log(num.toLocaleString());
+
+console.log(typeof strNum);
+console.log(typeof num);
+
+
+let singleQuote = 'It\'s a beautiful day!';
+let doubleQuote = "She said, \"Hello!\"";
+
+
+console.log(singleQuote);
+console.log(doubleQuote);
+console.log(templateLiteral);
+console.log(eval("2 + 2*2"));
+let templateLiteral = 'Priyanshu Jaiswal';
+console.log(templateLiteral);
+
+let group = ["Tony", "Banner", "Thor" ,"Strange","Clark"];
+group.push("Wayne");
+group.pop();
+group.shift();
+group.unshift();
+console.log(group);
+
+group.splice(1,0,"Sentry");
+console.log(group);
+let joined = group.join(",");
+console.log(joined);
+
+let transactions = [25,54,85,44,55 ];
+transactions.sort((a,b) => a - b);
+console.log(transactions);
+console.log(transactions.indexOf(48));
+console.log(transactions.includes(10));
+
+let matrix = [
+    [1,2,4],
+    [4,5,6],
+    [7,8,9]
+
+];
+console.log(matrix[1][2]);
+
+
+let today = new Date();
+console.log(today.toLocaleDateString());
+
+let specificDate = new Date(2025, 03, 16);
+console.log(specificDate);
+
+specificDate.setDate(specificDate.getDate() + 5);
+console.log(specificDate);
+
+console.log(Math.random());
+console.log(Math.floor(4.9));
+console.log(Math.ceil(4.1));
+console.log(Math.round(4.5));
+console.log(Math.pow(2,3));
+console.log(Math.sqrt(144));
+console.log(Math.cube(5));
+console.log(Math.max(1,5,10));
+
+let randomNum = Math.floor(Math.random()*100) + 1;
+console.log(randomNum);
+
+let a = 10;
+function changeValue(num){
+num = 20;
+}
+changeValue(a);
+console.log(a);
+
+let person = {name: "Mohit"};
+function changeName(obj){
+    obj.name = "Badal";
+}
+changeName(person);
+console.log(person.name);
+let person =  {name: "aman", age:25, city:"delhi"}
+for(let i=1; i<= 100; i++){
+    if(i == 69)continue;
+console.log("|=>" +i);
+
+}
+
+let count = 1;
+while(count<=5){
+    console.log(count);
+    count ++;
+}
+*/
